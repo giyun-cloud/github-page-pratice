5b3f521d386012c016b29c5d3edf6d513a12f494
giyun-cloud <nangiyoon@gmail.com>
2021-04-21 10:04:09 +0900

    end ver1

--- js/main.js
+++ js/main.js
@@ -11,3 +11,37 @@ function initMap() {
     title: 'GitHub'
   });
 }
+
+const toggleEl = document.getElementById('toggle-btn')
+const toggles = document.querySelectorAll('.toggle')
+
+let toggleClickTF = false
+toggleEl.addEventListener('click', function () {
+
+  // WAY - 1
+  // if(!toggleClickTF) {
+  //   for (let index = 0; index < toggles.length; index++) {
+  //     toggles[index].classList.add('on')
+  //   }
+  //   toggleClickTF = true
+  // }
+  // else {
+  //   for (let index = 0; index < toggles.length; index++) {
+  //     toggles[index].classList.remove('on')
+  //   }
+  //   toggleClickTF = false
+
+  // WAY -2
+  [].forEach.call(toggles, function(toggle) {
+    toggle.classList.toggle('on')
+  })
+  
+})
+
+window.addEventListener('resize', function () {
+  if(window.innerWidth > 1024) {
+    [].forEach.call(toggles, function(toggle) {
+      toggle.classList.remove('on')
+    })
+  }
+})
